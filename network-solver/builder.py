190ef7cbfcecfec29e7c5bc9834fa84a1ab0e91a
jinx <thorpete1@gmail.com>
2025-01-18 20:23:54 +1030

    implemeted  up to a manual itteration. Currently adding methods to automate the itteration process

--- network-solver/builder.py
+++ network-solver/builder.py
@@ -1,4 +1,8 @@
+
+from numbers import Number
+from pydoc import doc
 import numpy as np
+
 
 
 class Node:
@@ -7,11 +11,13 @@ class Node:
         self.ID = Node._nextID
         Node._nextID += 1
         self._edges = []
-        self.fixedPressure = False 
-        self.pressure = 1*10**5 # dmmy variable for development
+        
+        self.pressure = 1 # dmmy variable for development
+        
         self.height  = 0 
         self.demand = 0   
-      
+        self.fixedPressure = False 
+        
     def attachEdge(self,edge):
         if edge not in self._edges:
             self._edges.append(edge)
@@ -33,6 +39,7 @@ class Node:
     def setDemand(self,demand):
         self.demand = demand
             
+            
 
 class Edge:
     _nextID = 0
@@ -42,19 +49,25 @@ class Edge:
         self._nodeFrom = nodeFrom
         self._nodeTo = nodeTo
         Edge._nextID += 1 
+        self._network: Network = None
+        
+        self.flowrate = 0.1 # dmmy vale for development
+        self.previousFlowrate = self.flowrate # Initiallised to current flow rate
+        self.massFlowrate = None
+        
+        ## pipe related.
+        
         self.rougness = 1*10**-5  #dmmy vale for development
         self.length = 100 # dmmy vale for development
         self.hydraulicDiameter = None
-        self.area = None
-        self._network: Network = None
-        self.flowrate = 0.1 # dmmy vale for development
-        self.massFlowrate = None
+        self.area = None    
+        
         self.reynolds = None
         self.frictionFactor = 1
         self.resistance = None
         self.gravity = 9.81
         
-        self.setDiameter(0.1)  #dmmy vale for development
+        self.setDiameter(0.045)  #dmmy vale for development
         
     def attachFromNode(self):
         self._nodeFrom.attachEdge(self)
@@ -64,18 +77,18 @@ class Edge:
         
     def addNetwork(self,network):
         self._network = network
-        
+    
+    ## pipe related.
+    
     def setDiameter(self,diameter):
         self.hydraulicDiameter = diameter
         self.calculateArea()
-        
-        
+         
     def calculateArea(self):
         self.area = np.pi*self.hydraulicDiameter/4
         
     def updateReynolds(self):
         #self.reynolds = self.massFlowrate * self.hydraulicDiameter /(self._network._fluid.viscosity * self.area )
-        
         self.reynolds = self.flowrate * self.hydraulicDiameter * self._network._fluid.density /(self._network._fluid.viscosity * self.area)
          
     def updatefrictionFactor(self):
@@ -93,6 +106,8 @@ class Edge:
         self.resistance = (8 * self.frictionFactor * self.length) / (np.pi**2 * self.gravity *  self.hydraulicDiameter**5)
         return self.resistance            
 
+
+
 class Network:
     def __init__(self):
         self._nodes: list[Node] = []
@@ -100,6 +115,8 @@ class Network:
         self._unodes: list[Node] = []
         self._fnodes: list[Node] = []
         self._fluid: Fluid = None
+        self.itterationCount = 0
+        self.flowTolerance = 1e-4
                 
     def addEdge(self,edge):
         if edge not in self._edges:
@@ -118,7 +135,7 @@ class Network:
         self._fluid = fluid
                     
     def generateConnectivityMatrix(self):
-        # this is the A1 amtri
+        # this is the A1 matrix num edges x bum unkown pressure nodes
         self.connectivity = np.zeros((len(self._unodes),len(self._edges)))
         i = 0
         for n in self._unodes:
@@ -155,6 +172,13 @@ class Network:
         for e in self._edges:
             self.resistances[i,i] = e.updateResistance()
             i += 1
+    
+    def createUnkownHeadVector(self):
+        self.unkownHeadVector = np.zeros((len(self._unodes),1))
+        i = 0
+        for n in self._unodes:
+            self.unkownHeadVector[i] = n.pressure
+            i += 1
              
     def createFlowVector(self):
         self.flowVector = np.zeros((len(self._edges),1))
@@ -172,14 +196,66 @@ class Network:
     
     def generateGMatrix(self):
         # this is the G matrix
+        
+        # regenerate the resistance
+        self.generateResistanceMatrix()
+        
+        # then create the G matrix
         self.G = np.multiply(self.resistances,np.abs(self.flowVector[:]))
-                     
+        
+    def generateNMatrix(self):
+        # this is the n matrix, to start it will be all N =2 
+        self.N = np.eye(len(self._edges),len(self._edges))
+        
+    def createFixedHeadVector(self):
+        self.fixedHeadVector = np.zeros((len(self._fnodes),1))
+        i = 0
+        for n in self._fnodes:
+            self.fixedHeadVector[i] = n.pressure
+            i += 1
+            
+    def initialise(self):
+        """Initialises all the matrices and vectors required for the calculation.
+        """
+        
+        self.generateConnectivityMatrix()
+        self.generateFixedMatrix()
+        self.createFlowVector()
+        self.createDemandVector()
+        self.generateNMatrix()
+        self.createUnkownHeadVector()
+        self.createFixedHeadVector()
+    
+    def itterateHead(self):
+        # itterate the head value
+        schur = np.linalg.inv(self.connectivity.transpose() @ np.linalg.inv(self.G) @ self.connectivity)
+        
+        other = self.connectivity.transpose() @ ((1-2) * self.flowVector - np.linalg.inv(self.G) @ (self.fixed @ self.fixedHeadVector)) - 2 * self.demandVector
+        
+        self.unkownHeadVector = schur @ other
+        print(self.unkownHeadVector)
+        
+        i = 0
+        for n in self._unodes:
+            n.pressure = self.unkownHeadVector[i]
+            i += 1
+        
+    def itterateFlow(self):
+        # itterate flow (after itterating head)
+        self.flowVector = (1-1/2)*self.flowVector + (1/2)*np.linalg.inv(self.G) @ (self.connectivity @ self.unkownHeadVector + self.fixed @ self.fixedHeadVector)
+        print(self.flowVector)
+        
+    def itterate(self):
+        pass
+        
+        
 
 class Fluid:
     def __init__(self,density,viscosity):
         self.density = density
         self.viscosity = viscosity
         
+
 
 net0 = Network()     
 
@@ -196,7 +272,7 @@ e13 = Edge(n1,n3)
 
 n2.setDemand(10/3600)
 n3.setDemand(5/3600)
-n0.setFixedPressure(10)
+n0.setFixedPressure(20)
 
 net0.addFluid(fl0)
 net0.addNode(n0)
@@ -208,25 +284,30 @@ net0.addEdge(e01)
 net0.addEdge(e12)
 net0.addEdge(e13)
 
-print(len(net0._edges))
-print(len(net0._nodes))
-print(len(net0._unodes))
-print(len(net0._fnodes))
+# print(len(net0._edges))
+# print(len(net0._nodes))
+# print(len(net0._unodes))
+# print(len(net0._fnodes))
 
-net0.generateConnectivityMatrix()
-print(net0.connectivity)
+net0.initialise()
 
-net0.generateFixedMatrix()
-print(net0.fixed)
-
-net0.generateResistanceMatrix()
-print(net0.resistances)
-
-net0.createFlowVector()
 print(net0.flowVector)
+net0.generateGMatrix()
+net0.itterateHead()
+net0.itterateFlow()
 
 net0.generateGMatrix()
-print(net0.G)
+net0.itterateHead()
+net0.itterateFlow()
 
-net0.createDemandVector()
-print(net0.demandVector)
+net0.generateGMatrix()
+net0.itterateHead()
+net0.itterateFlow()
+
+net0.generateGMatrix()
+net0.itterateHead()
+net0.itterateFlow()
+
+net0.generateGMatrix()
+net0.itterateHead()
+net0.itterateFlow()
