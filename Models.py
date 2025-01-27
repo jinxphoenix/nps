07375d69748a7e83eb98d888b76c28af1129fb5d
jinx <thorpete1@gmail.com>
2025-01-27 22:55:48 +1030

    Completely rewrote the pipe class and added a partial pipe class so pump and pipe have a common super class. Implemented the pipe class, and debugged. Appears to be working. Note with regards to pump fit. Need to provide a decent intial guess when doing curve fitting for real, not just for random data.

--- Models.py
+++ Models.py
@@ -1,6 +1,12 @@
 
 from enum import Enum
 import numpy as np
+from Flows.types import PipeStatus
+
+class Fluid:
+    def __init__(self,density,viscosity):
+        self.density = density
+        self.viscosity = viscosity
 
 class Node:
     _nextID = 0
@@ -43,16 +49,23 @@ class Edge:
     # will need to pt a lot of the below into a type class (i.e. pipe, pmp, valve etc.)
     
     def __init__(self,nodeFrom, nodeTo):
+        
+        # Basic edge parameters
         self.ID = Edge._nextID
         self._nodeFrom: Node = nodeFrom
         self._nodeTo: Node = nodeTo
         Edge._nextID += 1 
         self._network: Network = None
         
+        # utility 
         self.status = None
+
+        # basic expected properties
         self.flowrate = 0.1 # dmmy vale for development
         self.previousFlowrate = self.flowrate - 1 # Initiallised to a value of not the prev flow rate so first itteration works
-        self.N_value = 1
+        self.exponent = None
+        self.resistance_offset = None
+        self.resistance = None  
                   
     def attachFromNode(self):
         self._nodeFrom.attachEdge(self)
@@ -63,88 +76,170 @@ class Edge:
     def addNetwork(self,network):
         self._network = network
         
-    def setExponent(self,*args):
+    def set_exponent(self,N):
         pass
     
-    def updatefrictionFactor(self):
+    def get_resistance_offset(self):
+        pass
+    
+    def updateResistance(self):
         pass   
  
 class Status(Enum):
     pass
-  
-class Pipe(Edge):
+
+class PartialPipe(Edge):
+    def __init__(self, nodeFrom, nodeTo):
+        super().__init__(nodeFrom, nodeTo)
+        
+        # nominal
+        self.diameter = None
+        self.area = None
+        self.velocity = None
+        self.reynolds = None
+        
+        # inlet
+        self.inletDiameter = None
+        self.inletArea = None
+        self.inletVelocity = None
+        self.inletReynolds = None
+        
+        # outlet
+        self.outletDiameter = None
+        self.outletArea = None
+        self.outletVelocity = None
+        self.outletReynolds = None
+        
+        # utilities values
+        self.gravity = 9.81
+        
+    @staticmethod    
+    def calculateArea(diameter: float):
+        return np.pi /4 * diameter**2 
+    
+    @staticmethod
+    def calculateVelocity(flowrate,area):
+        return flowrate / area
+    
+    @staticmethod
+    def calculateReynolds(fluid: Fluid, flowrate, charLength, area):
+        """Calculates the reynolds number for a full pipe.
+
+        Args:
+            fluid (Fluid): Object from network containing fluid properties
+            flowrate (_type_): flow rate in m3/s
+            charLength (_type_): characteristic length (i.e diameter)
+            area (_type_): cross sectional area
+
+        Returns:
+            _type_: reynolds number (unitless)
+        """
+        return flowrate * charLength * fluid.density /(fluid.viscosity * area)
+
+    def set_exponent(self, N):
+        self.exponent = N
+    
+class Pipe(PartialPipe):
     
     def __init__(self,nodeFrom,nodeTo,roughness,length,diameter):
         super().__init__(nodeFrom, nodeTo)
         ## pipe related.
         
+        self.set_diameter(diameter)
+        self.length = length   
         self.roughness = roughness 
-        self.length = length 
-        self.hydraulicDiameter = diameter
-        self.area = None    
-        
-        self.reynolds = None
-        self.frictionFactor = 1
-        self.resistance = None
-        self.gravity = 9.81
         self.k_value = 0
+        
+        # emergent from flow conditions
+        self.frictionFactor = 1     
         self.minorResistance = 0
         
-        self.calculateArea()
-        self.setExponent()
+        # set teh exponent to two as hard coded using darcey for pipes
+        self.set_exponent(2)
         
-    def setDiameter(self,diameter):
-        self.hydraulicDiameter = diameter
-        self.calculateArea()
+        # this is a pipe, so it has no resistance offset at zero flow
+        self.resistance_offset = 0
+        
+    def set_diameter(self,diameter):
+        self.diameter = diameter
+        self.inletDiameter = diameter
+        self.outletDiameter = diameter
+        
+        # update areas when the diameter is changed.
+        self.set_area()
          
-    def calculateArea(self):
-        self.area = np.pi*self.hydraulicDiameter/4
+    def set_area(self):
+        self.area = self.calculateArea(self.diameter)
+        self.inletArea = self.calculateArea(self.inletDiameter)
+        self.outletArea = self.calculateArea(self.outletDiameter)
         
-    def setKValue(self,k_value):
+    def set_k_value(self,k_value):
         self.k_value = k_value
-    
-    def setExponent(self):
-        self.N_value = 2
         
     def calculateMinorLosses(self):
-        self.minorResistance = self.k_value * 8 / (self.gravity * np.pi**2 * self.hydraulicDiameter**4)   
+        self.minorResistance = self.k_value * 8 / (self.gravity * np.pi**2 * self.diameter**4)   
         
-    def updateReynolds(self):
-        #self.reynolds = self.massFlowrate * self.hydraulicDiameter /(self._network._fluid.viscosity * self.area )
-        self.reynolds = self.flowrate * self.hydraulicDiameter * self._network._fluid.density /(self._network._fluid.viscosity * self.area)
+    def update_velocities(self):
+        pass
+    
+    def updateReynolds(self):      
+        self.reynolds = self.calculateReynolds(self._network._fluid,self.flowrate,self.diameter,self.area)
+        self.inletReynolds = self.calculateReynolds(self._network._fluid,self.flowrate,self.inletDiameter,self.inletArea)
+        self.outletReynolds = self.calculateReynolds(self._network._fluid,self.flowrate,self.outletDiameter,self.outletDiameter)
          
     def updatefrictionFactor(self):
         self.updateReynolds()
         if self.reynolds > 2000:
             # Use the colbrook Equation
-            a = self.roughness/(3.7*self.hydraulicDiameter)
+            a = self.roughness/(3.7*self.diameter)
             b = 2.51 / (self.roughness*np.sqrt(self.frictionFactor))
             c = -2*np.log10(a + b)
             self.frictionFactor = (1/c)**2
         else:
             # Use the laminar value
             self.frictionFactor = 64/self.reynolds
+    
+    def get_resistance_offset(self):
+        return self.resistance_offset
             
     def updateResistance(self):
         self.updatefrictionFactor()
         self.calculateMinorLosses()
-        self.resistance = (8 * self.frictionFactor * self.length) / (np.pi**2 * self.gravity *  self.hydraulicDiameter**5) + self.minorResistance
+        self.resistance = (8 * self.frictionFactor * self.length) / (np.pi**2 * self.gravity *  self.diameter**5) + self.minorResistance
         return self.resistance       
+   
+class Pump(PartialPipe):
+    def __init__(self, nodeFrom, nodeTo, alpha, beta, n):
+        super().__init__(nodeFrom, nodeTo)
+
+        # pump related
+        self.set_exponent(n)
+        self.set_flow_coefficient(alpha)
+        self.set_resistance_offset(beta)
+ 
+    def set_inlet_diameter(self,diameter):
+        self.inletDiameter = diameter
+        self.inletArea = self.calculateArea(diameter)
+        
+    def set_outlet_diameter(self,diameter):
+        self.outletDiameter = diameter
+        self.outletArea = self.calculateArea(diameter)
     
-class Pump(Edge): 
-    pass
-
-
+    def set_resistance_offset(self,value):
+        self.resistance_offset = value
+    
+    def get_resistance_offset(self):
+        return -self.resistance_offset
+    
+    def set_flow_coefficient(self,value):
+        self.flow_coefficient = value
+    
+    def updateResistance(self):
+        return -self.flow_coefficient
+        
 class Component(Edge):
     pass
-
-    
-class Fluid:
-    def __init__(self,density,viscosity):
-        self.density = density
-        self.viscosity = viscosity
             
-
 class Network:
     def __init__(self):
         self._nodes: list[Node] = []
@@ -156,12 +251,12 @@ class Network:
         self.flowTolerance = 1e-4
         self.pressureTolerance = 1e-4
                 
-    def addEdge(self,edge):
+    def addEdge(self,edge: Edge):
         if edge not in self._edges:
             self._edges.append(edge)
             edge.addNetwork(self)
             
-    def addNode(self,node):
+    def addNode(self,node: Node):
         if node not in self._nodes:
             self._nodes.append(node)
             if node.fixedPressure == False:
@@ -213,6 +308,15 @@ class Network:
             self.resistances[i,i] = e.updateResistance()
             i += 1
     
+    def generateResistanceOffsetMatrix(self):
+        # this creates a matrix of beta's, divided by the right amount of Q
+        pass
+        self.resistance_offsets = np.zeros((len(self._edges),len(self._edges)))
+        i = 0
+        for e in self._edges:
+            self.resistance_offsets[i,i] = e.get_resistance_offset()
+            i += 1
+    
     def createUnkownHeadVector(self):
         self.unkownHeadVector = np.zeros((len(self._unodes),1))
         i = 0
@@ -237,17 +341,27 @@ class Network:
     def generateA11Matrix(self):
         # this is the A11 matrix, which is the resistances multipled by the prev itteration flows
         self.generateResistanceMatrix()
+        self.generateResistanceOffsetMatrix()
+        
         Qabs = np.abs(self.flowVector)
+        Npow = np.diag(self.N)[:,np.newaxis]
+        Qpow = np.power(Qabs,(Npow-1))
+        A11naked = np.multiply(self.resistances, Qpow[:])
+        B11 = np.multiply(self.resistance_offsets, (1/self.flowVector)[:])
         
         # create the diagonal matrix by multiplying row wise, the resistance matrix with the abs flow vector
-        self.A11 = np.multiply(self.resistances, Qabs[:])
+        self.A11 = A11naked + B11
+        # print(self.A11)
         
     def generateGMatrix(self):
         # this is the G matrix (it is A11 frotn matrix multiplied with the N matrix.)
         self.generateA11Matrix()
         
+        # create the A11* matrix
+        A11_star = self.A11 - np.multiply(self.resistance_offsets, (1/self.flowVector)[:])
+        
         # then create the G matrix
-        self.G = self.N @ self.A11
+        self.G = self.N @ A11_star
         # print(f'G vector:\n{self.G}')
         
     def generateNMatrix(self):
@@ -260,7 +374,7 @@ class Network:
         self.N = np.eye(len(self._edges),len(self._edges))
         i = 0
         for e in self._edges:
-            self.N[i,i] = e.N_value
+            self.N[i,i] = e.exponent
             i += 1
         
     def createFixedHeadVector(self):
@@ -292,7 +406,10 @@ class Network:
         print(f'max pressure residual is: {max_res}')
         return max_res
         
-        
+    def setInitalConditions(self):
+        """Sets all flow and pressure values to values sutiable to start calculation
+        """
+        pass    
                 
     def initialise(self):
         """Initialises all the matrices and vectors required for the calculation.
@@ -319,7 +436,7 @@ class Network:
                     
         # note n = 2 at the moment. This needs to be refactored to use the n matrix and N matrix function.
         A = self.connectivity.transpose() @ np.linalg.inv(self.G) @ self.connectivity
-        b = -( self.connectivity.transpose() @ np.linalg.inv(self.G) @ (self.A11 @ self.flowVector + self.fixed @ self.fixedHeadVector) - (self.connectivity.transpose() @ self.flowVector - self.demandVector)  )
+        b = -( self.connectivity.transpose() @ np.linalg.inv(self.G) @ (self.A11 @ self.flowVector + self.fixed @ self.fixedHeadVector) -- (self.connectivity.transpose() @ self.flowVector - self.demandVector)  )
                
         # print(f"A:\n {A}")
         # print(f"b:\n {b}")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from operator import ne
-from Models import Node, Pipe, Fluid, Network
+from Models import Node, Pipe, Fluid, Network, Pump
 
 # Set up the network
 net0 = Network()     
@@ -11,35 +11,37 @@ n0 = Node()
 n1 = Node()
 n2 = Node()
 n3 = Node()
-n4 = Node()
-n5 = Node()
+# n4 = Node()
+# n5 = Node()
 
-e01 = Pipe(n0,n1,1*10**-5,100,0.05)
-e12 = Pipe(n1,n2,1*10**-5,100,0.05)
-e13 = Pipe(n1,n3,1*10**-5,100,0.05)
-e14 = Pipe(n1,n4,1*10**-5,100,0.05)
-e45 = Pipe(n4,n5,1*10**-5,100,0.05)
+e01 = Pipe(n0,n1,1*10**-5,100,0.1)
+# e12 = Pipe(n1,n2,1*10**-5,100,0.05)
+e12 = Pump(n1,n2,-6680.879290080472,49.93200286913462,1.493199982498533)
+e23 = Pipe(n2,n3,1*10**-5,100,0.1)
+# e14 = Pipe(n1,n4,1*10**-5,100,0.05)
+# e45 = Pipe(n4,n5,1*10**-5,100,0.05)
 
-n2.setDemand(20/3600)
-n3.setDemand(5/3600)
-
+# n2.setDemand(20/3600)
+# n3.setDemand(80/3600)
 n0.setFixedPressure(20)
-n5.setFixedPressure(10)
-e14.setKValue(2)
+n3.setFixedPressure(30)
+# n5.setFixedPressure(5)
+# e14.set_k_value(2)
 
 net0.addFluid(fl0)
 net0.addNode(n0)
 net0.addNode(n1)
 net0.addNode(n2)
 net0.addNode(n3)
-net0.addNode(n4)
-net0.addNode(n5)
+# net0.addNode(n4)
+# net0.addNode(n5)
 
 net0.addEdge(e01)
 net0.addEdge(e12)
-net0.addEdge(e13)
-net0.addEdge(e14)
-net0.addEdge(e45)
+net0.addEdge(e23)
+# net0.addEdge(e13)
+# net0.addEdge(e14)
+# net0.addEdge(e45)
 
 # initialise and run the simulation
 
